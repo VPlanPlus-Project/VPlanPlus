9e93d7931198d0b59cec4f2444f7d35568908b6d
Julius Babies <julius@familie-babies.de>
2023-10-23 21:33:19 +0200

    - better preview style

--- app/src/main/java/es/jvbabi/vplanplus/ui/screens/onboarding/WelcomeScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/onboarding/WelcomeScreen.kt
@@ -1,21 +1,9 @@
 package es.jvbabi.vplanplus.ui.screens.onboarding
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import es.jvbabi.vplanplus.R
 import es.jvbabi.vplanplus.ui.screens.Screen
 import es.jvbabi.vplanplus.ui.screens.onboarding.common.OnboardingScreen
