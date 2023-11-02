e587009712c5f949e669c419f3f14b8c14319d7b
Julius Babies <julius@familie-babies.de>
2023-11-02 22:02:19 +0100

    - show profiles

--- app/src/main/java/es/jvbabi/vplanplus/ui/screens/settings/profile/ProfileManagementScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/settings/profile/ProfileManagementScreen.kt
@@ -1,37 +1,78 @@
 package es.jvbabi.vplanplus.ui.screens.settings.profile
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import es.jvbabi.vplanplus.R
+
+@Composable
+fun ProfileManagementScreen(
+    navController: NavHostController,
+    viewModel: ProfileManagementViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    LaunchedEffect("Init") {
+        if (state.schools.isEmpty() && !state.isLoading) viewModel.init()
+    }
+
+    ProfileManagementScreenContent(
+        { navController.popBackStack() },
+        state
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileManagementScreen(
-    navController: NavHostController
+fun ProfileManagementScreenContent(
+    onBackClicked: () -> Unit = {},
+    state: ProfileManagementState
 ) {
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.profileManagement_title)) },
                 navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
+                    IconButton(onClick = { onBackClicked() }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Default.ArrowBack,
                             contentDescription = stringResource(id = R.string.back)
@@ -48,14 +89,155 @@ fun ProfileManagementScreen(
                 .padding(pv)
                 .padding(horizontal = 16.dp)
         ) {
-            Text(text = "Profile Management Screen")
+            LazyColumn {
+                items (state.schools.sortedBy { it.name }) { school ->
+                    Card(
+                        colors = CardDefaults.cardColors(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(
+                                text = school.name,
+                                modifier = Modifier
+                                    .padding(16.dp),
+                                textAlign = TextAlign.Center,
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .horizontalScroll(rememberScrollState())
+                                    .padding(start = 16.dp, bottom = 16.dp),
+                            ) {
+                                school.profiles.forEach {
+                                    ProfileCard(type = it.type, name = it.name)
+                                }
+                                ProfileCard(type = -1, name = "+")
+                            }
+                        }
+                    }
+                }
+            }
         }
 
     }
 }
 
 @Composable
+fun ProfileCard(type: Int, name: String) {
+    Card(
+        colors = CardDefaults.cardColors(),
+        border = if (type != -1) BorderStroke(1.dp, Color.Black) else null,
+        modifier = Modifier
+            .padding(end = 16.dp)
+            .size(width = 80.dp, height = 80.dp)
+            .dashedBorder(if (type == -1) 2.dp else 0.dp, Color.Black, 8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                if (type != -1) {
+                    Text(text = name, style = MaterialTheme.typography.headlineSmall)
+                    Text(text = when (type) {
+                        0 -> "Klasse"
+                        1 -> "Lehrer"
+                        2 -> "Raum"
+                        else -> "Unbekannt"
+                    }, style = MaterialTheme.typography.bodyMedium)
+                } else {
+                    Text(text = "+", style = MaterialTheme.typography.headlineMedium)
+                }
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun ProfileCardPreview() {
+    ProfileCard(0, "7a")
+}
+
+@Composable
 @Preview(showBackground = true)
 fun ProfileManagementScreenPreview() {
-    ProfileManagementScreen(navController = rememberNavController())
+    ProfileManagementScreenContent(
+        state = ProfileManagementState(
+            listOf(
+                ProfileManagementSchool(
+                    "Waldschule",
+                    listOf(
+                        ProfileManagementProfile(
+                            id = 0,
+                            name = "7a",
+                            type = 0
+                        ),
+                        ProfileManagementProfile(
+                            id = 1,
+                            name = "207",
+                            type = 2
+                        ),
+                        ProfileManagementProfile(
+                            id = 3,
+                            name = "Mul",
+                            type = 1
+                        )
+                    )
+                ),
+                ProfileManagementSchool(
+                    "200.009. Oberschule Dresden",
+                    listOf(
+                        ProfileManagementProfile(
+                            id = 4,
+                            name = "Mul",
+                            type = 1
+                        ),
+                    )
+                )
+            )
+        )
+    )
+}
+
+// https://stackoverflow.com/a/67039676/16682019
+fun Modifier.dashedBorder(strokeWidth: Dp, color: Color, cornerRadiusDp: Dp): Modifier {
+    if (strokeWidth == 0.dp) return this
+    return composed(
+        factory = {
+            val density = LocalDensity.current
+            val strokeWidthPx = density.run { strokeWidth.toPx() }
+            val cornerRadiusPx = density.run { cornerRadiusDp.toPx() }
+
+            this.then(
+                Modifier.drawWithCache {
+                    onDrawBehind {
+                        val stroke = Stroke(
+                            width = strokeWidthPx,
+                            pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                        )
+
+                        drawRoundRect(
+                            color = color,
+                            style = stroke,
+                            cornerRadius = CornerRadius(cornerRadiusPx)
+                        )
+                    }
+                }
+            )
+        }
+    )
 }
---
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/settings/profile/ProfileManagementViewModel.kt
@@ -0,0 +1,75 @@
+package es.jvbabi.vplanplus.ui.screens.settings.profile
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import es.jvbabi.vplanplus.domain.usecase.ClassUseCases
+import es.jvbabi.vplanplus.domain.usecase.ProfileUseCases
+import es.jvbabi.vplanplus.domain.usecase.SchoolUseCases
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileManagementViewModel @Inject constructor(
+    private val profileUseCases: ProfileUseCases,
+    private val classUseCases: ClassUseCases,
+    private val schoolUseCases: SchoolUseCases
+): ViewModel() {
+
+    private val _state = mutableStateOf(ProfileManagementState())
+    val state: State<ProfileManagementState> = _state
+
+    suspend fun init() {
+        _state.value = _state.value.copy(isLoading = true)
+        val dbProfiles = profileUseCases.getProfiles()
+        val schools = mutableMapOf<String, List<ProfileManagementProfile>>()
+        dbProfiles.forEach {
+            when (it.type) {
+                0 -> {
+                    val `class` = classUseCases.getClassById(it.referenceId)
+                    val school = schoolUseCases.getSchoolFromId(`class`.schoolId)
+                    if (schools.containsKey(school.name)) {
+                        schools[school.name] = schools[school.name]!!.plus(
+                            ProfileManagementProfile(
+                                id = it.id!!,
+                                name = it.name,
+                                type = it.type
+                            )
+                        )
+                    } else {
+                        schools[school.name] = listOf(
+                            ProfileManagementProfile(
+                                id = it.id!!,
+                                name = it.name,
+                                type = it.type
+                            )
+                        )
+                    }
+                }
+            }
+        }
+
+        _state.value = _state.value.copy(isLoading = false, schools = schools.map {
+            ProfileManagementSchool(
+                name = it.key,
+                profiles = it.value
+            )
+        })
+    }
+}
+
+data class ProfileManagementState(
+    val schools: List<ProfileManagementSchool> = emptyList(),
+    val isLoading: Boolean = false
+)
+
+data class ProfileManagementSchool(
+    val name: String,
+    val profiles: List<ProfileManagementProfile>
+)
+
+data class ProfileManagementProfile(
+    val id: Long,
+    val name: String,
+    val type: Int
+)
