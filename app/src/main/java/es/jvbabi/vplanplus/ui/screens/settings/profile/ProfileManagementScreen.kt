ef414bf0baafdaee9774b05a5212cd60400ca2ab
Julius Babies <julius@familie-babies.de>
2023-11-02 21:15:02 +0100

    - top bar

--- app/src/main/java/es/jvbabi/vplanplus/ui/screens/settings/profile/ProfileManagementScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/settings/profile/ProfileManagementScreen.kt
@@ -1,17 +1,61 @@
 package es.jvbabi.vplanplus.ui.screens.settings.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import es.jvbabi.vplanplus.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileManagementScreen(navController: NavHostController) {
-    Text(text = "ProfileManagementScreen")
+fun ProfileManagementScreen(
+    navController: NavHostController
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.profileManagement_title)) },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = stringResource(id = R.string.back)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors()
+            )
+        }
+    ) { pv ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(pv)
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(text = "Profile Management Screen")
+        }
+
+    }
 }
 
 @Composable
 @Preview(showBackground = true)
 fun ProfileManagementScreenPreview() {
-
+    ProfileManagementScreen(navController = rememberNavController())
 }
