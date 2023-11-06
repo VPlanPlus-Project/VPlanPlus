90b5c97c87b962ceaa4765c3147836eb422d60b9
Julius Babies <julius@familie-babies.de>
2023-11-06 20:16:08 +0100

    - fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2-alpha"
+        versionName = "0.3-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
