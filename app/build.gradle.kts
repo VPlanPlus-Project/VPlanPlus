a3d4e76dfd2595f95d3eed257861e20a2152ca4b
Julius Babies <julius@familie-babies.de>
2023-11-08 19:40:46 +0100

    - 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.4-alpha"
+        versionName = "0.4.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
