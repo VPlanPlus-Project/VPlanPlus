4fb01d6de79bd090370d2ea6d933c071369ce04d
Julius Babies <julius@familie-babies.de>
2023-11-11 17:44:35 +0100

    - show rooms

--- app/src/main/java/es/jvbabi/vplanplus/ui/screens/home/search/room/RoomSearch.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/home/search/room/RoomSearch.kt
@@ -1,17 +1,31 @@
 package es.jvbabi.vplanplus.ui.screens.home.search.room
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,8 +77,77 @@ fun FindAvailableRoomScreenContent(
             Column(
                 modifier = Modifier.padding(horizontal = 16.dp)
             ) {
-                Text(text = "Schule: ${state.currentSchool?.name}, Stunde ${state.currentLesson}")
+                Text(text = "Schule: ${state.currentSchool?.name}")
                 Text(text = "Um Freie Räume anderer Schulen zu sehen, wechsle dein aktives Profil.")
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Column {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .horizontalScroll(rememberScrollState())
+                        ) {
+                            repeat(12) { lessonNumber ->
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(50.dp)
+                                        .padding(horizontal = 4.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                        .background(MaterialTheme.colorScheme.primaryContainer)
+                                        .weight(1f, false),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.SpaceAround
+                                ) {
+                                    if (lessonNumber > 0) {
+                                        Text(
+                                            text = "${lessonNumber - 1}",
+                                            color = MaterialTheme.colorScheme.onPrimaryContainer
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        Column(
+                            modifier = Modifier.verticalScroll(rememberScrollState())
+                        ) {
+                            state.rooms.forEach {
+                                Row {
+                                    Column(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(50.dp)
+                                            .padding(horizontal = 4.dp)
+                                            .clip(RoundedCornerShape(8.dp))
+                                            .background(MaterialTheme.colorScheme.primaryContainer)
+                                            .weight(1f, false),
+                                        verticalArrangement = Arrangement.SpaceAround
+                                    ) {
+                                        Text(
+                                            text = it.key,
+                                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                                            style = MaterialTheme.typography.labelSmall,
+                                            modifier = Modifier.rotate(90f)
+                                        )
+                                    }
+                                    it.value.forEach { available ->
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .height(50.dp)
+                                                .padding(horizontal = 4.dp)
+                                                .clip(RoundedCornerShape(8.dp))
+                                                .background(if (available) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.error)
+                                                .weight(1f, false),
+                                            horizontalAlignment = Alignment.CenterHorizontally,
+                                            verticalArrangement = Arrangement.SpaceAround
+                                        ) {
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
