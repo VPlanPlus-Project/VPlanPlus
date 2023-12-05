b9f5f0874a983cd904fe65cc37a195fb8e1de3b8
Julius Vincent Babies <julius@familie-babies.de>
2023-12-05 22:23:04 +0100

    - icon

--- app/src/main/java/es/jvbabi/vplanplus/ui/common/SubjectIcon.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/common/SubjectIcon.kt
@@ -6,7 +6,7 @@ import androidx.compose.material.icons.filled.Bolt
 import androidx.compose.material.icons.filled.Book
 import androidx.compose.material.icons.filled.Brush
 import androidx.compose.material.icons.filled.Calculate
-import androidx.compose.material.icons.filled.DesktopWindows
+import androidx.compose.material.icons.filled.Code
 import androidx.compose.material.icons.filled.EventBusy
 import androidx.compose.material.icons.filled.HistoryEdu
 import androidx.compose.material.icons.filled.MusicNote
@@ -28,7 +28,7 @@ import es.jvbabi.vplanplus.R
 fun SubjectIcon(subject: String, modifier: Modifier, tint: Color = Color.Unspecified) {
     when (subject.lowercase()) {
         "deutsch", "de", "deu" -> Icon(imageVector = Icons.Default.Book, contentDescription = null, modifier = modifier, tint = tint)
-        "informatik", "inf", "info" -> Icon(imageVector = Icons.Default.DesktopWindows, contentDescription = null, modifier = modifier, tint = tint)
+        "informatik", "inf", "info" -> Icon(imageVector = Icons.Default.Code, contentDescription = null, modifier = modifier, tint = tint)
         "biologie", "bio" -> Icon(imageVector = Icons.Default.Biotech, contentDescription = null, modifier = modifier, tint = tint)
         "mathematik", "mathe", "ma" -> Icon(imageVector = Icons.Default.Calculate, contentDescription = null, modifier = modifier, tint = tint)
         "grw" -> Icon(imageVector = Icons.Default.Payment, contentDescription = null, modifier = modifier, tint = tint)
