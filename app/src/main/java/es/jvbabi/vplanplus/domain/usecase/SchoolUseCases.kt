8cca1496e692161f996f60acd0ec193e83326a9f
Julius Vincent Babies <julius@familie-babies.de>
2023-12-20 18:15:49 +0100

    - merge

--- app/src/main/java/es/jvbabi/vplanplus/domain/usecase/SchoolUseCases.kt
+++ app/src/main/java/es/jvbabi/vplanplus/domain/usecase/SchoolUseCases.kt
@@ -7,16 +7,8 @@ class SchoolUseCases(
     private val schoolRepository: SchoolRepository
 ) {
 
-    fun checkSchoolId(schoolId: String): SchoolIdCheckResult {
-        return if (schoolId.length == 8 && schoolId.toLongOrNull() != null) SchoolIdCheckResult.SYNTACTICALLY_CORRECT else SchoolIdCheckResult.INVALID
-    }
-
     suspend fun deleteSchool(schoolId: Long) {
         schoolRepository.deleteSchool(schoolId)
-    }
-
-    suspend fun checkSchoolIdOnline(schoolId: Long): Flow<SchoolIdCheckResult?> {
-        return flowOf(schoolRepository.checkSchoolId(schoolId))
     }
 
     suspend fun getSchoolByName(schoolName: String): School {
