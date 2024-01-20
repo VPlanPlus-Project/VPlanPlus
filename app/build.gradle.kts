49b9a4c1c9044ad5ca95752acbfcdf952fbc2f30
Julius Vincent Babies <julius@familie-babies.de>
2024-01-20 22:04:46 +0100

    - added warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 34
-        versionCode = 34
-        versionName = "0.8.3-alpha-dev-11"
+        versionCode = 35
+        versionName = "0.8.3-alpha-dev-12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
