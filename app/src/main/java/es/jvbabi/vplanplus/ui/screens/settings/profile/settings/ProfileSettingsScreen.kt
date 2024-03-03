70e7da3791fb365ce81bedaa0b8b941746df1f8a
Julius Vincent Babies <julius@familie-babies.de>
2024-03-03 14:02:44 +0100

    - scroll fix

--- app/src/main/java/es/jvbabi/vplanplus/ui/screens/settings/profile/settings/ProfileSettingsScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/settings/profile/settings/ProfileSettingsScreen.kt
@@ -1,7 +1,10 @@
 package es.jvbabi.vplanplus.ui.screens.settings.profile.settings
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -191,7 +194,12 @@ private fun ProfileSettingsScreenContent(
                 },
             )
         }
-        Column(modifier = Modifier.padding(paddingValues = paddingValues)) {
+        Column(
+            modifier = Modifier
+                .padding(paddingValues = paddingValues)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
             BigButtonGroup(
                 buttons = listOf(
                     BigButton(
