9e97bb12c28cad814ecce22d46cfe45fac5058b1
Julius Vincent Babies <julius@familie-babies.de>
2024-03-09 18:14:17 +0100

    - toggle filter

--- app/src/main/java/es/jvbabi/vplanplus/feature/grades/ui/view/GradesScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/feature/grades/ui/view/GradesScreen.kt
@@ -7,9 +7,18 @@ import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,8 +28,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Fingerprint
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -32,6 +43,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,6 +67,7 @@ import es.jvbabi.vplanplus.feature.grades.ui.view.components.screens.NoVppId
 import es.jvbabi.vplanplus.feature.grades.ui.view.components.screens.WrongProfile
 import es.jvbabi.vplanplus.ui.common.BackIcon
 import es.jvbabi.vplanplus.ui.common.InfoCard
+import es.jvbabi.vplanplus.ui.common.SubjectIcon
 import es.jvbabi.vplanplus.ui.screens.Screen
 import java.nio.charset.StandardCharsets
 import kotlin.io.encoding.Base64
@@ -116,7 +131,9 @@ fun GradesScreen(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class,
+    ExperimentalAnimationGraphicsApi::class
+)
 @Composable
 private fun GradesScreenContent(
     onBack: () -> Unit,
@@ -131,11 +148,28 @@ private fun GradesScreenContent(
     state: GradesState,
     navBar: @Composable () -> Unit
 ) {
+    var searchExpanded by rememberSaveable { mutableStateOf(false) }
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.grades_title)) },
-                navigationIcon = { IconButton(onClick = onBack) { BackIcon() } }
+                navigationIcon = { IconButton(onClick = onBack) { BackIcon() } },
+                actions = {
+                    IconButton(onClick = { searchExpanded = !searchExpanded }) {
+                        val painter = rememberAnimatedVectorPainter(
+                            animatedImageVector = AnimatedImageVector.animatedVectorResource(R.drawable.anim_search_close),
+                            atEnd = searchExpanded
+                        )
+                        Image(
+                            painter = painter,
+                            contentDescription = null,
+                            modifier = Modifier.scale(.8f),
+                            colorFilter =
+                                if (isSystemInDarkTheme()) ColorFilter.colorMatrix(ColorMatrix(colorMatrix))
+                                else null
+                        )
+                    }
+                }
             )
         },
         bottomBar = navBar
@@ -149,6 +183,31 @@ private fun GradesScreenContent(
                 GradeUseState.NO_VPP_ID -> NoVppId(onLinkVppId)
                 GradeUseState.WRONG_PROFILE_SELECTED -> WrongProfile()
                 else -> {}
+            }
+            AnimatedVisibility(
+                visible = searchExpanded,
+                enter = expandVertically(tween(200)),
+                exit = shrinkVertically(tween(200))
+            ) {
+                FlowRow(
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    state.grades.keys.sortedBy { it.name }.forEach { subject ->
+                        FilterChip(
+                            selected = false,
+                            onClick = { /*TODO*/ },
+                            label = { Text(text = subject.short) },
+                            modifier = Modifier.padding(horizontal = 4.dp),
+                            leadingIcon = {
+                                SubjectIcon(
+                                    subject = subject.name,
+                                    modifier = Modifier,
+                                    tint = MaterialTheme.colorScheme.onSurface
+                                )
+                            }
+                        )
+                    }
+                }
             }
             AnimatedVisibility(
                 visible = state.showEnableBiometricBanner,
@@ -260,3 +319,10 @@ fun GradesScreenPreview() {
         onDisableBiometric = {}
     )
 }
+
+private val colorMatrix = floatArrayOf(
+    -1f, 0f, 0f, 0f, 255f,
+    0f, -1f, 0f, 0f, 255f,
+    0f, 0f, -1f, 0f, 255f,
+    0f, 0f, 0f, 1f, 0f
+)
