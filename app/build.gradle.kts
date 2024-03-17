1378b85fb9708b8428ba8ae75884d5c3dacd725c
Julius Vincent Babies <julius@familie-babies.de>
2024-03-17 20:41:16 +0100

    - custom tabs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 34
-        versionCode = 109
-        versionName = "v1.1-dev4"
+        versionCode = 110
+        versionName = "v1.1-dev5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
