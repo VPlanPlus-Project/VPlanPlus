1d9ee7cb65faca7d026da4f59e063d3fc6a61a65
Julius Vincent Babies <julius@familie-babies.de>
2024-03-19 18:45:39 +0100

    - version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 34
-        versionCode = 111
-        versionName = "v1.1-dev6"
+        versionCode = 112
+        versionName = "v1.1-dev7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
