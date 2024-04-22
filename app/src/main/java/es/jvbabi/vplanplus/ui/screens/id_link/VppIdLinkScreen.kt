51ea467f8a46d4645f55faf129b083e0a1daa1c2
Julius Vincent Babies <julius@familie-babies.de>
2024-04-22 20:45:44 +0200

    - update profiles on login

--- app/src/main/java/es/jvbabi/vplanplus/ui/screens/id_link/VppIdLinkScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/id_link/VppIdLinkScreen.kt
@@ -2,6 +2,7 @@ package es.jvbabi.vplanplus.ui.screens.id_link
 
 import android.content.Intent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -24,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +35,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import es.jvbabi.vplanplus.R
+import es.jvbabi.vplanplus.domain.model.Profile
 import es.jvbabi.vplanplus.domain.model.VppId
 import es.jvbabi.vplanplus.ui.preview.ProfilePreview
 import es.jvbabi.vplanplus.ui.screens.Screen
@@ -46,7 +51,7 @@ fun VppIdLinkScreen(
     token: String?,
     vppIdLinkViewModel: VppIdLinkViewModel = hiltViewModel()
 ) {
-    val state = vppIdLinkViewModel.state.value
+    val state = vppIdLinkViewModel.state
 
     LaunchedEffect(token) {
         vppIdLinkViewModel.init(token)
@@ -63,6 +68,7 @@ fun VppIdLinkScreen(
         onRetry = {
             vppIdLinkViewModel.init(token)
         },
+        onToggleProfile = vppIdLinkViewModel::onToggleProfileState,
         onContactUs = { userId, className ->
             val intent = Intent(Intent.ACTION_SENDTO)
             intent.data = android.net.Uri.parse("mailto:")
@@ -89,6 +95,7 @@ private fun VppIdLinkScreenContent(
     onOk: () -> Unit = {},
     onRetry: () -> Unit = {},
     onContactUs: (userId: Int?, className: String?) -> Unit = { _, _ -> },
+    onToggleProfile: (profile: Profile) -> Unit = {},
     state: VppIdLinkState
 ) {
     Box(
@@ -189,6 +196,26 @@ private fun VppIdLinkScreenContent(
                         modifier = Modifier.padding(vertical = 8.dp),
                         className = state.vppId.classes.name
                     )
+                } else {
+                    Column(Modifier.padding(vertical = 8.dp)) {
+                        Text(text = stringResource(id = R.string.vppIdLink_selectAProfile), style = MaterialTheme.typography.labelLarge)
+                        state.selectedProfiles.forEach { (profile, selected) ->
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 4.dp)
+                                    .clip(RoundedCornerShape(8.dp))
+                                    .clickable { onToggleProfile(profile) },
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Checkbox(checked = selected, onCheckedChange = { onToggleProfile(profile) })
+                                Text(
+                                    text = profile.displayName,
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                        }
+                    }
                 }
                 Button(enabled = state.selectedProfiles.any { it.value } || state.selectedProfiles.isEmpty(), onClick = { onOk() }) {
                     Text(text = stringResource(id = android.R.string.ok))
--- app/src/main/java/es/jvbabi/vplanplus/ui/screens/id_link/VppIdLinkViewModel.kt
+++ app/src/main/java/es/jvbabi/vplanplus/ui/screens/id_link/VppIdLinkViewModel.kt
@@ -1,8 +1,9 @@
 package es.jvbabi.vplanplus.ui.screens.id_link
 
 import android.util.Log
-import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,29 +19,29 @@ class VppIdLinkViewModel @Inject constructor(
     private val vppIdLinkUseCases: VppIdLinkUseCases,
 ) : ViewModel() {
 
-    private val _state = mutableStateOf(VppIdLinkState())
-    val state: State<VppIdLinkState> = _state
+    var state by mutableStateOf(VppIdLinkState())
+        private set
 
     private var token: String = ""
 
     fun init(token: String?) {
         Log.i("vpp.ID Link", "Init with $token")
         if (token == null) {
-            _state.value = _state.value.copy(isLoading = false)
+            state = state.copy(isLoading = false)
             return
         }
         this.token = token
         viewModelScope.launch {
-            _state.value = _state.value.copy(isLoading = true, error = false)
+            state = state.copy(isLoading = true, error = false)
             val response = vppIdLinkUseCases.getVppIdDetailsUseCase(token)
             if (response.data == null) {
                 Log.d("vpp.ID Link", "Something went wrong: ${response.response}")
-                _state.value = _state.value.copy(error = true, isLoading = false)
+                state = state.copy(error = true, isLoading = false)
                 return@launch
             }
             if (response.data.classes == null) {
                 // class does not exists
-                _state.value = _state.value.copy(
+                state = state.copy(
                     isLoading = false,
                     classNotFound = true,
                     error = true,
@@ -52,7 +53,7 @@ class VppIdLinkViewModel @Inject constructor(
 
             if (profiles.size == 1) vppIdLinkUseCases.setProfileVppIdUseCase(mapOf(profiles.first() to true), response.data)
 
-            _state.value = _state.value.copy(
+            state = state.copy(
                 isLoading = false,
                 vppId = response.data,
                 response = response.response,
@@ -64,8 +65,13 @@ class VppIdLinkViewModel @Inject constructor(
 
     fun onProceed() {
         viewModelScope.launch {
-            if (state.value.selectedProfiles.isEmpty()) vppIdLinkUseCases.updateMissingLinksStateUseCase()
+            if (state.selectedProfiles.isEmpty()) vppIdLinkUseCases.updateMissingLinksStateUseCase()
+            else vppIdLinkUseCases.setProfileVppIdUseCase(state.selectedProfiles, state.vppId!!)
         }
+    }
+
+    fun onToggleProfileState(profile: Profile) {
+        state = state.copy(selectedProfiles = state.selectedProfiles.plus(profile to !(state.selectedProfiles[profile] ?: true)))
     }
 }
 
