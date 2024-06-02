40b3e59721ff14852a633f40eaccacdfd8d17cc6
Julius Babies <julius@familie-babies.de>
2024-06-02 13:24:41 +0200

    - removed no default lesson dialog in add homework (#535)

--- app/src/main/java/es/jvbabi/vplanplus/feature/main_homework/add/ui/AddHomeworkScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/feature/main_homework/add/ui/AddHomeworkScreen.kt
@@ -43,10 +43,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,7 +52,6 @@ import androidx.navigation.NavHostController
 import es.jvbabi.vplanplus.R
 import es.jvbabi.vplanplus.domain.model.DefaultLesson
 import es.jvbabi.vplanplus.feature.main_homework.add.ui.components.DateChip
-import es.jvbabi.vplanplus.feature.main_homework.add.ui.components.NoDefaultLessonDialog
 import es.jvbabi.vplanplus.feature.main_homework.shared.domain.repository.HomeworkModificationResult
 import es.jvbabi.vplanplus.ui.common.DOT
 import es.jvbabi.vplanplus.ui.common.InfoCard
@@ -130,9 +125,6 @@ private fun AddHomeworkContent(
     onSave: () -> Unit = {},
     state: AddHomeworkState
 ) {
-
-    var showNoDefaultLessonDialog by rememberSaveable { mutableStateOf(false) }
-    if (showNoDefaultLessonDialog) NoDefaultLessonDialog(onYes = { showNoDefaultLessonDialog = false; onSave() }, onNo = { showNoDefaultLessonDialog = false })
 
     val noTeacher = stringResource(id = R.string.settings_profileDefaultLessonNoTeacher)
     if (state.isLessonDialogOpen) {
@@ -377,7 +369,7 @@ private fun AddHomeworkContent(
             }
 
             Button(
-                onClick = { if (state.selectedDefaultLesson == null) showNoDefaultLessonDialog = true else onSave() },
+                onClick = onSave,
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxWidth(),
