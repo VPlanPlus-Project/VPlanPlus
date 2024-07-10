db7b9a6264444279ea71d3946d5ae98515e099e9
Julius Vincent Babies <julvin.babies+github@gmail.com>
2024-07-10 22:17:49 +0200

    - version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 34
-        versionCode = 215
+        versionCode = 216
         versionName = "${LocalDate.now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))}_534"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
