c950abacbcfdc0fc8ca9649be2a9add5484c8eaa
Julius Vincent Babies <julvin.babies+github@gmail.com>
2024-07-21 16:17:56 +0200

    Migration: Default Server: Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 225
-        versionName = "${LocalDate.now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))}_577"
+        versionCode = 226
+        versionName = "${LocalDate.now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))}_SJ24-25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
