36b9566e54446daf4f79ee3b998a93640ae30ce0
Julius Vincent Babies <julvin.babies+github@gmail.com>
2024-09-01 11:24:11 +0200

    Animate items on details 2

--- app/src/main/java/es/jvbabi/vplanplus/feature/main_calendar/home/ui/components/Day.kt
+++ app/src/main/java/es/jvbabi/vplanplus/feature/main_calendar/home/ui/components/Day.kt
@@ -23,20 +23,36 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
 import es.jvbabi.vplanplus.util.blendColor
 import java.time.LocalDate
 import java.time.Month
+
+private const val DOT_WIDTH_DP = 4
+private const val DOT_HEIGHT_DP = 4
+private const val CAPSULE_THICKNESS_DP = 12
+private const val CAPSULE_TEXT_PADDING_DP = 4
 
 /**
  * A single day in the calendar.
@@ -57,7 +73,10 @@ fun Day(
 ) {
     val localDensity = LocalDensity.current
     var width by remember { mutableFloatStateOf(0f) }
-    var widthDp by remember(width) { mutableStateOf(localDensity.run { width.toDp() }) }
+    val widthDp by remember(width) { mutableStateOf(localDensity.run { width.toDp() }) }
+
+    val dpToPx: Dp.() -> Float = { localDensity.run { toPx() } }
+
     Column(
         modifier = Modifier
             .clip(RoundedCornerShape(8.dp))
@@ -101,14 +120,15 @@ fun Day(
                     DayDisplayState.SMALL -> normalColor
                     DayDisplayState.REGULAR -> blendColor(normalColor, colorRegardingSelectedMonth, progress)
                     else -> colorRegardingSelectedMonth
-                },
-
-                )
+                }
+            )
         }
         Box(
             modifier = Modifier
                 .then(
-                    if (state == DayDisplayState.DETAILED) Modifier.weight(16f)
+                    if (state == DayDisplayState.DETAILED) Modifier
+                        .height(16.dp + (widthDp - 16.dp) * progress)
+                        .rotate(90f * progress)
                     else Modifier.height(16.dp)
                 )
         ) {
@@ -119,18 +139,40 @@ fun Day(
                         else Modifier.width(32.dp)
                     )
                     .fillMaxHeight(),
-                horizontalArrangement = Arrangement.Center,
-                verticalArrangement = Arrangement.Top,
-
+                horizontalArrangement = Arrangement.Start,
+                verticalArrangement = Arrangement.Top
             ) {
                 repeat(homework) {
+                    val textMeasurer = rememberTextMeasurer()
+                    val measuredText =
+                        textMeasurer.measure(
+                            AnnotatedString("HA"),
+                            constraints = Constraints.fixedWidth((width - (2 * CAPSULE_TEXT_PADDING_DP.dp.dpToPx())).toInt().coerceAtLeast(0)),
+                            style = MaterialTheme.typography.labelSmall,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+
                     Box(
                         modifier = Modifier
                             .padding(1.dp)
                             .then(
                                 if (state == DayDisplayState.DETAILED) Modifier
-                                    .height(4.dp + progress * 8.dp)
-                                    .width(4.dp + (widthDp - 4.dp) * progress)
+                                    .width(DOT_WIDTH_DP.dp + progress * (CAPSULE_THICKNESS_DP - DOT_WIDTH_DP).dp)
+                                    .height(DOT_HEIGHT_DP.dp + (widthDp - DOT_HEIGHT_DP.dp) * progress)
+                                    .drawWithContent {
+                                        drawContent()
+                                        rotate(
+                                            degrees = -90f,
+                                            pivot = Offset(0f, 0f)
+                                        ) {
+                                            scale(progress, progress, Offset(-size.height + CAPSULE_TEXT_PADDING_DP.dp.dpToPx(), size.width / 2 - measuredText.size.height / 2)) {
+                                                translate(left = -size.height + CAPSULE_TEXT_PADDING_DP.dp.dpToPx(), top = size.width / 2 - measuredText.size.height / 2) {
+                                                    drawText(measuredText, color = blendColor(Color.Transparent, Color.White, progress))
+                                                }
+                                            }
+                                        }
+                                    }
                                 else Modifier.size(4.dp)
                             )
                             .clip(RoundedCornerShape(50))
@@ -138,13 +180,36 @@ fun Day(
                     )
                 }
                 repeat(exams) {
+                    val textMeasurer = rememberTextMeasurer()
+                    val measuredText =
+                        textMeasurer.measure(
+                            AnnotatedString("Test/KA"),
+                            constraints = Constraints.fixedWidth((width - (2 * CAPSULE_TEXT_PADDING_DP.dp.dpToPx())).toInt().coerceAtLeast(0)),
+                            style = MaterialTheme.typography.labelSmall,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+
                     Box(
                         modifier = Modifier
                             .padding(1.dp)
                             .then(
                                 if (state == DayDisplayState.DETAILED) Modifier
-                                    .height(4.dp + progress * 8.dp)
-                                    .width(4.dp + (widthDp - 4.dp) * progress)
+                                    .width(DOT_WIDTH_DP.dp + progress * (CAPSULE_THICKNESS_DP - DOT_WIDTH_DP).dp)
+                                    .height(DOT_HEIGHT_DP.dp + (widthDp - DOT_HEIGHT_DP.dp) * progress)
+                                    .drawWithContent {
+                                        drawContent()
+                                        rotate(
+                                            degrees = -90f,
+                                            pivot = Offset(0f, 0f)
+                                        ) {
+                                            scale(progress, progress, Offset(-size.height + CAPSULE_TEXT_PADDING_DP.dp.dpToPx(), size.width / 2 - measuredText.size.height / 2)) {
+                                                translate(left = -size.height + CAPSULE_TEXT_PADDING_DP.dp.dpToPx(), top = size.width / 2 - measuredText.size.height / 2) {
+                                                    drawText(measuredText, color = blendColor(Color.Transparent, Color.White, progress))
+                                                }
+                                            }
+                                        }
+                                    }
                                 else Modifier.size(4.dp)
                             )
                             .clip(RoundedCornerShape(50))
