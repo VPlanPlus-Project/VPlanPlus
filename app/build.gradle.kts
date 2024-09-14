64c2b8a3a9e504ef7cde7b22ec1d4c7f949a8e01
Julius Babies <julius@familie-babies.de>
2024-09-14 19:59:47 +0200

    Version VPP_20240914-android-insider1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 262
+        versionCode = 263
         versionName = "VPP_${LocalDate.now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))}-android-insider1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
