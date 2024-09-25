bd01d36c95be4dbef9eb9315e8b736814f3ed3cf
Julius Babies <julius@familie-babies.de>
2024-09-25 19:05:40 +0200

    idk

--- app/src/main/java/es/jvbabi/vplanplus/feature/main_home/ui/HomeScreen.kt
+++ app/src/main/java/es/jvbabi/vplanplus/feature/main_home/ui/HomeScreen.kt
@@ -4,24 +4,38 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.NoAccounts
+import androidx.compose.material.icons.filled.Today
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -41,6 +55,9 @@ import es.jvbabi.vplanplus.feature.main_homework.add.ui.AddHomeworkSheet
 import es.jvbabi.vplanplus.feature.main_homework.add.ui.AddHomeworkSheetInitialValues
 import es.jvbabi.vplanplus.feature.settings.vpp_id.ui.onLogin
 import es.jvbabi.vplanplus.ui.common.InfoCard
+import es.jvbabi.vplanplus.ui.common.RowVerticalCenter
+import es.jvbabi.vplanplus.ui.common.Spacer4Dp
+import es.jvbabi.vplanplus.ui.common.Spacer8Dp
 import es.jvbabi.vplanplus.ui.common.keyboardAsState
 import es.jvbabi.vplanplus.ui.common.openLink
 import es.jvbabi.vplanplus.ui.preview.GroupPreview
@@ -50,7 +67,9 @@ import es.jvbabi.vplanplus.ui.preview.ProfilePreview.toActiveVppId
 import es.jvbabi.vplanplus.ui.preview.SchoolPreview
 import es.jvbabi.vplanplus.ui.preview.VppIdPreview
 import es.jvbabi.vplanplus.ui.screens.Screen
+import java.time.LocalDate
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun HomeScreen(
@@ -78,7 +97,11 @@ fun HomeScreen(
             navHostController.navigate("${Screen.SettingsProfileScreen.route}/${it.id}")
         },
         onOpenNews = { homeViewModel.onMenuOpenedChange(false); navHostController.navigate(Screen.NewsScreen.route) },
-        onOpenSettings = { homeViewModel.onMenuOpenedChange(false); navHostController.navigate(Screen.SettingsScreen.route) },
+        onOpenSettings = {
+            homeViewModel.onMenuOpenedChange(false); navHostController.navigate(
+            Screen.SettingsScreen.route
+        )
+        },
         onPrivacyPolicyClicked = {
             openLink(
                 context,
@@ -92,7 +115,11 @@ fun HomeScreen(
             )
         },
         onOpenSearch = { navHostController.navigate(Screen.SearchScreen.route) },
-        onRefreshClicked = { homeViewModel.onMenuOpenedChange(false); homeViewModel.onRefreshClicked(context) },
+        onRefreshClicked = {
+            homeViewModel.onMenuOpenedChange(false); homeViewModel.onRefreshClicked(
+            context
+        )
+        },
         onFixVppIdSessionClicked = { onLogin(context, state.server) },
         onFixVppIdLinksClicked = { navHostController.navigate(Screen.SettingsVppIdScreen.route) },
         onIgnoreInvalidVppIdSessions = homeViewModel::ignoreInvalidVppIdSessions,
@@ -137,7 +164,11 @@ fun HomeScreenContent(
         onCloseUntilNextVersion = { onVersionHintsClosed(true) }
     )
 
-    var addHomeworkSheetInitialValues by rememberSaveable<MutableState<AddHomeworkSheetInitialValues?>> { mutableStateOf(null) }
+    var addHomeworkSheetInitialValues by rememberSaveable<MutableState<AddHomeworkSheetInitialValues?>> {
+        mutableStateOf(
+            null
+        )
+    }
     if (addHomeworkSheetInitialValues != null) {
         AddHomeworkSheet(
             onClose = { addHomeworkSheetInitialValues = null },
@@ -145,7 +176,7 @@ fun HomeScreenContent(
         )
     }
 
-    val lazyListState = rememberLazyListState()
+    val pagerState = rememberPagerState { 2 }
 
     Scaffold(
         bottomBar = { navBar(!keyboardAsState().value) },
@@ -160,46 +191,101 @@ fun HomeScreenContent(
                 onProfileClicked = { onOpenMenu(true) },
                 onSearchClicked = onOpenSearch,
             )
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize(),
-                state = lazyListState
-            ) {
-                item appHead@{
-                    Collapsable(
-                        expand = state.hasMissingVppIdToProfileLinks || state.hasInvalidVppIdSession
-                    ) { ImportantHeader(Modifier.padding(horizontal = 16.dp)) }
-                    Collapsable(expand = state.hasInvalidVppIdSession) {
-                        InfoCard(
-                            imageVector = Icons.Default.NoAccounts,
-                            title = stringResource(id = R.string.home_invalidVppIdSessionTitle),
-                            text = stringResource(id = R.string.home_invalidVppIdSessionText),
-                            buttonText1 = stringResource(id = R.string.ignore),
-                            buttonAction1 = onIgnoreInvalidVppIdSessions,
-                            buttonText2 = stringResource(id = R.string.fix),
-                            buttonAction2 = onFixVppIdSessionClicked,
-                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                    }
-                    Collapsable(expand = state.hasMissingVppIdToProfileLinks) {
-                        MissingVppIdLinkToProfileCard(
-                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-                            onFixClicked = onFixVppIdLinksClicked
-                        )
-                    }
-                    BadCredentialsBanner(
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-                        expand = state.currentProfile.getSchool().credentialsValid == false,
-                        onFixCredentialsClicked = onFixCredentialsClicked
-                    )
+            Collapsable(
+                expand = state.hasMissingVppIdToProfileLinks || state.hasInvalidVppIdSession
+            ) { ImportantHeader(Modifier.padding(horizontal = 16.dp)) }
+            Collapsable(expand = state.hasInvalidVppIdSession) {
+                InfoCard(
+                    imageVector = Icons.Default.NoAccounts,
+                    title = stringResource(id = R.string.home_invalidVppIdSessionTitle),
+                    text = stringResource(id = R.string.home_invalidVppIdSessionText),
+                    buttonText1 = stringResource(id = R.string.ignore),
+                    buttonAction1 = onIgnoreInvalidVppIdSessions,
+                    buttonText2 = stringResource(id = R.string.fix),
+                    buttonAction2 = onFixVppIdSessionClicked,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+            }
+            Collapsable(expand = state.hasMissingVppIdToProfileLinks) {
+                MissingVppIdLinkToProfileCard(
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                    onFixClicked = onFixVppIdLinksClicked
+                )
+            }
+            BadCredentialsBanner(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                expand = state.currentProfile.getSchool().credentialsValid == false,
+                onFixCredentialsClicked = onFixCredentialsClicked
+            )
 
-                    QuickActions(
-                        modifier = Modifier.padding(vertical = 8.dp),
-                        onNewHomeworkClicked = { addHomeworkSheetInitialValues = AddHomeworkSheetInitialValues() },
-                        onFindAvailableRoomClicked = onBookRoomClicked,
-                        onSendFeedback = onSendFeedback,
-                        allowHomeworkQuickAction = (state.currentProfile as? ClassProfile)?.isHomeworkEnabled ?: false
-                    )
+            QuickActions(
+                modifier = Modifier.padding(vertical = 8.dp),
+                onNewHomeworkClicked = {
+                    addHomeworkSheetInitialValues = AddHomeworkSheetInitialValues()
+                },
+                onFindAvailableRoomClicked = onBookRoomClicked,
+                onSendFeedback = onSendFeedback,
+                allowHomeworkQuickAction = (state.currentProfile as? ClassProfile)?.isHomeworkEnabled
+                    ?: false
+            )
+
+            Spacer8Dp()
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.Top
+            ) {
+                if (it == 0) { // today
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 16.dp)
+                            .shadow(elevation = 2.dp, shape = RoundedCornerShape(topStart = 16.dp, bottomStart = 16.dp))
+                            .clip(RoundedCornerShape(topStart = 16.dp, bottomStart = 16.dp))
+                            .background(MaterialTheme.colorScheme.surface)
+                    ) {
+                        Column(Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+                            RowVerticalCenter {
+                                Icon(
+                                    imageVector = Icons.Default.Today,
+                                    contentDescription = null,
+                                    modifier = Modifier.size(24.dp),
+                                    tint = MaterialTheme.colorScheme.primary
+                                )
+                                Spacer8Dp()
+                                Text(
+                                    text = "Tagesplanung für heute",
+                                    style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Normal),
+                                )
+                            }
+                            Text(
+                                text = LocalDate.now()
+                                    .format(DateTimeFormatter.ofPattern("EEEE, dd.MM.yyyy")),
+                                style = MaterialTheme.typography.labelMedium,
+                            )
+                        }
+                        Column(
+                            modifier = Modifier
+                                .weight(1f, true)
+                                .verticalScroll(rememberScrollState())
+                        ) {
+
+                        }
+                        Column {
+                            TextButton(onClick = {}) {
+                                RowVerticalCenter {
+                                    Text("Kalender")
+                                    Spacer4Dp()
+                                    Icon(Icons.Default.CalendarMonth, contentDescription = null)
+                                }
+                            }
+                        }
+                    }
+                } else { // next
+
                 }
             }
         }
@@ -229,7 +315,10 @@ fun HomeScreenContent(
 private fun HomeScreenPreview() {
     val school = SchoolPreview.generateRandomSchools(1).first()
     val group = GroupPreview.generateGroup(school)
-    val profile = ProfilePreview.generateClassProfile(group, VppIdPreview.generateVppId(group).toActiveVppId())
+    val profile = ProfilePreview.generateClassProfile(
+        group,
+        VppIdPreview.generateVppId(group).toActiveVppId()
+    )
     HomeScreenContent(
         navBar = navBar,
         state = HomeState(
