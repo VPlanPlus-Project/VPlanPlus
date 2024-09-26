32d39e85f32d54890aa3b3c4d39411560b6338f4
Julius Babies <julius@familie-babies.de>
2024-09-26 07:16:52 +0200

    fix merged for dev channels
    VPP_20240926-android-insider1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 274
+        versionCode = 275
         versionName = "VPP_${LocalDate.now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))}-android-insider1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
