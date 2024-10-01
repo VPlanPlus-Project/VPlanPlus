25f25223fca2e088d90f31b5f3d944bb7e0799ab
Julius Babies <julius@familie-babies.de>
2024-10-01 15:09:03 +0200

    delete onboarding keys

--- app/src/main/java/es/jvbabi/vplanplus/feature/onboarding/stages/c_credentials/domain/usecase/CheckCredentialsAndInitOnboardingForSchoolUseCase.kt
+++ app/src/main/java/es/jvbabi/vplanplus/feature/onboarding/stages/c_credentials/domain/usecase/CheckCredentialsAndInitOnboardingForSchoolUseCase.kt
@@ -10,6 +10,7 @@ import es.jvbabi.vplanplus.domain.repository.ProfileRepository
 import es.jvbabi.vplanplus.domain.repository.SchoolRepository
 import es.jvbabi.vplanplus.domain.repository.VPlanRepository
 import es.jvbabi.vplanplus.domain.repository.VppIdRepository
+import es.jvbabi.vplanplus.feature.onboarding.stages.h_setup.domain.usecase.onboardingSetupKeys
 import kotlinx.coroutines.flow.first
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
@@ -28,7 +29,7 @@ class CheckCredentialsAndInitOnboardingForSchoolUseCase(
 ) {
     suspend operator fun invoke(sp24SchoolId: Int, username: String, password: String): OnboardingInit? {
         val school = schoolRepository.getSchoolBySp24Id(sp24SchoolId)
-
+        onboardingSetupKeys.forEach { keyValueRepository.delete(it) }
 
         val baseDataResponse = baseDataRepository.getBaseData(sp24SchoolId, username, password)
         when (baseDataResponse) {
