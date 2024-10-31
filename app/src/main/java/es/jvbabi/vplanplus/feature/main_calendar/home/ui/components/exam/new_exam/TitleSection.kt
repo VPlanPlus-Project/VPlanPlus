ef4a90d3f5a0baae110df4345726c030b27a597b
Julius Babies <julius@familie-babies.de>
2024-10-31 10:34:47 +0100

    New Exam auto focus title

--- app/src/main/java/es/jvbabi/vplanplus/feature/main_calendar/home/ui/components/exam/new_exam/TitleSection.kt
+++ app/src/main/java/es/jvbabi/vplanplus/feature/main_calendar/home/ui/components/exam/new_exam/TitleSection.kt
@@ -1,5 +1,6 @@
 package es.jvbabi.vplanplus.feature.main_calendar.home.ui.components.exam.new_exam
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -8,7 +9,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
@@ -22,12 +27,15 @@ fun AddExamTitleSection(
     currentTitle: String,
     onUpdateTitle: (title: String) -> Unit
 ) {
+    val focusRequester = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
     AddExamItem(
         iconContainerSize = 40.dp
     ) {
         TextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester),
             value = currentTitle,
             onValueChange = onUpdateTitle,
             placeholder = { Text(text = stringResource(R.string.examsNew_topic), style = MaterialTheme.typography.headlineMedium, color = Color.Gray) },
@@ -42,6 +50,11 @@ fun AddExamTitleSection(
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
             keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
         )
+    }
+
+    LaunchedEffect(Unit) {
+        try { focusRequester.requestFocus() }
+        catch (e: Exception) { Log.i("AddExamTitleSection", "Focus request failed") }
     }
 }
 
