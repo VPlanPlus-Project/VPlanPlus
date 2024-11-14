551f121edf9759a2c03cd28f6ec7490f03d37117
Julius Babies <julius@familie-babies.de>
2024-11-14 21:18:54 +0100

    Version
    VPP_20241114-android-open1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 320
+        versionCode = 327
         versionName = "VPP_${LocalDate.now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))}-android-open1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
