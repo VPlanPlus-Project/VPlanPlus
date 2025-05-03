8b2f12b31f7eb8dcdc035cbffe4246a53b50458c
Julius Vincent Babies <julius@familie-babies.de>
2025-05-03 22:55:07 +0200

    Bump versionCode to 340

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "es.jvbabi.vplanplus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 333
+        versionCode = 340
         versionName = "VPP_${LocalDate.now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))}-android-insider1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
